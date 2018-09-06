6d36e51380d8e015a63b4aca5acf7030ba9086db
LilyVL <s098448@ap.be>
2018-09-06 15:26:34 +0200

    Added http request to joke api

--- src/main/java/edu/ap/spring/controller/JokeController.java
+++ src/main/java/edu/ap/spring/controller/JokeController.java
@@ -1,11 +1,20 @@
 package edu.ap.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import javax.json.Json;
+import javax.json.JsonReader;
+import javax.json.JsonObject;
+import javax.json.JsonValue;
+import java.io.StringReader;
 
 import edu.ap.spring.jpa.JokeRepository;
+import edu.ap.spring.jpa.Joke;
 
 @Controller
 @Scope("session")
@@ -13,12 +22,20 @@ public class JokeController {
    
    public JokeController() {
    }
-       
+   
+   @Autowired
+   JokeRepository repository;   
+   
    @RequestMapping("/joke")
    public String joke() {
 	   return "joke";
    }
-		   
+		
+   @RequestMapping("/list")
+   public String list() {
+	   return "list";
+   }   
+   
    @RequestMapping("/joke_post")
    public String joke_post() {
 	   return "";
@@ -35,14 +52,23 @@ public class JokeController {
 		   				 Model model) {
 
       model.addAttribute("firstName", firstName);
-      model.addAttribute("lastName", lastName);
+      model.addAttribute("lastName", lastName);      
       
-      
-      
+      RestTemplate restTemplate = new RestTemplate();
+      String reply = restTemplate.getForObject("http://api.icndb.com/jokes/random?firstName=" + firstName + "&lastName=" + lastName, String.class);
+      System.out.println(reply);
       //model.addAttribute("joke", joke);
       
-      //repository.save(new Joke(firstName, lastName, joke));
+      JsonReader reader = Json.createReader(new StringReader(reply));
+      JsonObject object = reader.readObject();
+      JsonObject innerReply = object.getJsonObject("value");
+      String joke = innerReply.getString("joke");
+      System.out.println(joke);
+      reader.close();
+
+      model.addAttribute("joke", joke);
+      repository.save(new Joke(firstName, lastName, joke));
       
-      return "joke_post";
+      return "joke";
    }
 }
